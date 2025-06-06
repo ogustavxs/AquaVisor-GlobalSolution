c5a401e19beaaa89b6f2eccefc6b7affa3406b68
ogustavxs <gustavopessoasillva@gmail.com>
2025-06-06 19:34:22 -0300

    feat: implementado tema claro e escuro, adicionado menu hamburguer e  links atualizados no HTML

--- scripts/script.js
+++ scripts/script.js
@@ -51,6 +51,44 @@ function nextImage(){
     document.getElementById("radio"+count).checked = true
 }
 
+// Botão hamburguer
+const hamburguerButton = document.getElementById('hamburguer-button');
+const hamburguerMenu = document.querySelector('.hamburguer-menu');
+
+hamburguerButton.addEventListener('click', () => {
+    if (window.innerWidth >= 1090) {
+        hamburguerMenu.style.display = 'none';
+        return;
+    }
+    hamburguerMenu.style.display = hamburguerMenu.style.display === 'block' ? 'none' : 'block';
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 1090) {
+        hamburguerMenu.style.display = 'none';
+    }
+});
+
+
+// Modos de cores
+const themeButtons = document.querySelectorAll('.theme-btn');
+const body = document.body;
+
+themeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const theme = button.dataset.theme; // 'dark' e 'light'
+
+        body.classList.remove('modo-escuro', 'modo-claro');
+        
+        if (theme === 'dark') {
+            body.classList.add('modo-escuro');
+        } else if (theme === 'light') {
+            body.classList.add('modo-claro');
+        }
+
+    });
+});
+
 // Quiz
 
 
@@ -138,3 +176,4 @@ function mostrarPergunta(){
         respostaBotao.appendChild(botao);
     });
 }*/
+
